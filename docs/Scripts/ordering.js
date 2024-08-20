b31932222e26362fc7690fff467b79d15f48829c
MemeFrug <maxwell.knight@ntschools.net>
2024-08-20 14:28:56 +0930

    Continued on implementing ordering system, started interaction event for the User interface

--- docs/Scripts/ordering.js
+++ docs/Scripts/ordering.js
@@ -2,7 +2,40 @@ console.log("loaded ordering.js");
 
 // Set up the ordering UI
 
-function setUpOrdering() {
+const buttons = {
+    food: document.getElementById("foodButton"),
+    drinks: document.getElementById("drinksButton"),
+    other: document.getElementById("otherButton"),
+    submit: document.getElementById("submitButton")
+}
+
+const orderingUI = {
+    //Initialise
+    init: () => {
+        buttons.submit.addEventListener("mouseup", orderingUI.submit)
+        buttons.food.addEventListener("mouseup", () => orderingUI.changeMenu(0))
+        buttons.drinks.addEventListener("mouseup", () => orderingUI.changeMenu(1))
+        buttons.other.addEventListener("mouseup", () => orderingUI.changeMenu(2))
+    },
+    changeMenu: (menu) => {
+        /*
+        // menu is food if = 0
+        // menu is drinks if = 1
+        // menu is other if = 2
+        */
+        console.log("Changing menu to", menu);
+    },
+    submit: () => {
+        console.log("Submitting");
+
+    }
+}
+
+function setUpOrdering(itemsList) {
+    const column1 = document.getElementById("column1");
+    const column2 = document.getElementById("column2");
+    const column3 = document.getElementById("column3");
+
 
 }
 
@@ -12,3 +45,5 @@ function debugToggleOrdering() {
                                 Game.interface.toggleUserInterface()
                                 Game.camera.draw = !Game.camera.draw // Prevent drawing under the UI 
 }
+
+orderingUI.init()
